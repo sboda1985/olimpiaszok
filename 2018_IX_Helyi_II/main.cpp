1d108f33dcd25105ad1c1d9d9ebbc105b8b210a3
sboda1985 <bodaszilard@gmail.com>
2018-02-06 13:29:16 +0200

    main.cpp - feladat leírás

--- 2018_IX_Helyi_II/main.cpp
+++ 2018_IX_Helyi_II/main.cpp
@@ -1,3 +1,16 @@
+
+/*
+Egy kódot egy n és k szám segítségével generálnak, a következőképpen:
+* az első sorban a két első tag 1 es, az ezután következők ez előzőek összegei
+* a következő k-1 sorban az első tag az előtte levő sorból az első két tag összege
+* a következő k-1 sorban a második tag az előtte levő sorból a második és harmadik tag összege
+* a következő k-1 sorban az ezután következő tagok az előző kettő összegei.
+* a kód a k. sorban lévő n. tag lesz, ha az prím, ha nem, akkor az ezután következő első prímszám
+
+Trükk: valójában a Fibonnacci sort kell legeneráljuk, méghozzá a Fibbonacci sornak a F(n+2*k-2) tagját, 
+ha ez prím, ha nem, akkor a következő prím tagját
+
+*/
 #include <iostream>
 #include <fstream>
 
